6e5f9f33c90fa9b31ae713736d617936fdc13a2a
Eduard Musatov <edikmusatov15@gmail.com>
2024-02-23 20:26:15 +0300

    Update backend references to backend-php

--- src/components/FormAuth.js
+++ src/components/FormAuth.js
@@ -3,117 +3,117 @@ import { useForm } from 'react-hook-form';
 
 function FormAuth({ onSwitchToRegister, onSwitchToPersonalAccount, onLoginSuccess }) {
 
-   // Хуки useState
-   const [showPassword, setShowPassword] = useState(false);
+	// Хуки useState
+	const [showPassword, setShowPassword] = useState(false);
 
-   const togglePasswordVisibility = () => {
-      setShowPassword(!showPassword);
-   };
+	const togglePasswordVisibility = () => {
+		setShowPassword(!showPassword);
+	};
 
-   // Хук useForm
-   const {
-      register,
-      formState: { errors, isValid },
-      handleSubmit
-   } = useForm({
-      mode: "onBlur"
-   });
+	// Хук useForm
+	const {
+		register,
+		formState: { errors, isValid },
+		handleSubmit
+	} = useForm({
+		mode: "onBlur"
+	});
 
-   // функция обратного вызова (та функция, которую можно передать как параметр в другую функцию)
-   const onSubmit = (data) => {
-      // передача на сервер json строки
-      fetch('http://backend/index.php/user/auth', {
-         method: 'POST',
-         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-         },
-         body: JSON.stringify(data)
-      })
-         .then(response => {
-            // проверяем код состояния ответа (http reponse code status)
-            const label_password = document.getElementById("label_password")
-            const label_login = document.getElementById("label_login")
-            switch (response.status) {
-               case 200:
-                  const loginFromInput = data['login'] // получаем правильный, подтвержденный логин из input
-                  onLoginSuccess(loginFromInput)
-                  onSwitchToPersonalAccount() // переключение на личный кабинет
-                  return
-               case 403:
-                  label_login.textContent = ""
-                  label_password.textContent = "Неверный пароль."
-                  return
-               case 404:
-                  label_password.textContent = ""
-                  label_login.textContent = "Пользователя не существует."
-                  return
-            }
-         })
-      // .then(response => response.json()) // преобразуем ответ в json
-      // .then(response => console.log(response))
-   }
+	// функция обратного вызова (та функция, которую можно передать как параметр в другую функцию)
+	const onSubmit = (data) => {
+		// передача на сервер json строки
+		fetch('http://backend-php/index.php/user/auth', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: JSON.stringify(data)
+		})
+			.then(response => {
+				// проверяем код состояния ответа (http reponse code status)
+				const label_password = document.getElementById("label_password")
+				const label_login = document.getElementById("label_login")
+				switch (response.status) {
+					case 200:
+						const loginFromInput = data['login'] // получаем правильный, подтвержденный логин из input
+						onLoginSuccess(loginFromInput)
+						onSwitchToPersonalAccount() // переключение на личный кабинет
+						return
+					case 403:
+						label_login.textContent = ""
+						label_password.textContent = "Неверный пароль."
+						return
+					case 404:
+						label_password.textContent = ""
+						label_login.textContent = "Пользователя не существует."
+						return
+				}
+			})
+		// .then(response => response.json()) // преобразуем ответ в json
+		// .then(response => console.log(response))
+	}
 
-   return (
+	return (
 
-      <div style={{ marginTop: '100px' }}>
-         {/* функция handleSubmit принимает как параметр callback функцию */}
-         <form onSubmit={handleSubmit(onSubmit)} className="form">
+		<div style={{ marginTop: '100px' }}>
+			{/* функция handleSubmit принимает как параметр callback функцию */}
+			<form onSubmit={handleSubmit(onSubmit)} className="form">
 
-            <div className="form__title">Форма авторизации</div>
+				<div className="form__title">Форма авторизации</div>
 
-            {/* Login */}
-            <label>
-               <div className="label">
-                  <div className="label__title">Логин</div>
-                  <input
-                     {...register('login', {
-                        required: 'Логин не указан'
-                     })}
-                     type="text"
-                     autoComplete="off"
-                  />
+				{/* Login */}
+				<label>
+					<div className="label">
+						<div className="label__title">Логин</div>
+						<input
+							{...register('login', {
+								required: 'Логин не указан'
+							})}
+							type="text"
+							autoComplete="off"
+						/>
 
-               </div>
+					</div>
 
-               <div id="label_login" style={{ height: 40 }}>
-                  {errors?.login && <p className="error">{errors?.login?.message || "Error!"}</p>}
-               </div>
-            </label>
+					<div id="label_login" style={{ height: 40 }}>
+						{errors?.login && <p className="error">{errors?.login?.message || "Error!"}</p>}
+					</div>
+				</label>
 
-            {/* Password */}
-            <label>
-               <div className="password__input">
-                  <div>Пароль</div>
+				{/* Password */}
+				<label>
+					<div className="password__input">
+						<div>Пароль</div>
 
-                  <input
-                     {...register('password', {
-                        required: 'Пароль не указан'
-                     })}
-                     type={showPassword ? 'text' : 'password'}
-                     autoComplete="off"
-                  />
+						<input
+							{...register('password', {
+								required: 'Пароль не указан'
+							})}
+							type={showPassword ? 'text' : 'password'}
+							autoComplete="off"
+						/>
 
-                  <img className="show" onClick={togglePasswordVisibility} src={showPassword ? "/img/eye.svg" : "/img/eye-off.svg"} />
+						<img className="show" onClick={togglePasswordVisibility} src={showPassword ? "/img/eye.svg" : "/img/eye-off.svg"} />
 
-               </div>
+					</div>
 
-               <div id="label_password" style={{ height: 40 }}>
-                  {errors?.password && <p className="error">{errors?.password?.message || "Error!"}</p>}
-               </div>
-            </label>
+					<div id="label_password" style={{ height: 40 }}>
+						{errors?.password && <p className="error">{errors?.password?.message || "Error!"}</p>}
+					</div>
+				</label>
 
-            {/* SUBMIT BUTTON */}
-            <input className="submit_button" type="submit" value="Авторизоваться" disabled={!isValid} />
+				{/* SUBMIT BUTTON */}
+				<input className="submit_button" type="submit" value="Авторизоваться" disabled={!isValid} />
 
 
-            <div className="switch">
-               <div>Еще не зарегестрированы?</div>
-               <div className="link" onClick={onSwitchToRegister}>Зарегестрироваться.</div>
-            </div>
+				<div className="switch">
+					<div>Еще не зарегестрированы?</div>
+					<div className="link" onClick={onSwitchToRegister}>Зарегестрироваться.</div>
+				</div>
 
-         </form>
-      </div>
-   )
+			</form>
+		</div>
+	)
 }
 
 export default FormAuth;
--- src/components/PersonalAccount.js
+++ src/components/PersonalAccount.js
@@ -2,56 +2,56 @@ import { useEffect, useState } from "react";
 
 function PersonalAccount({ onLogOut, loggedInUser }) {
 
-   const [userName, setUserName] = useState('')
-   // ЭТО НАДО ИЗМЕНИТЬ МБ
-   const [theme, setTheme] = useState('light')
+	const [userName, setUserName] = useState('')
+	// ЭТО НАДО ИЗМЕНИТЬ МБ
+	const [theme, setTheme] = useState('light')
 
-   useEffect(() => {
-      fetch('http://backend/index.php/user/userinfo/' + loggedInUser, {
-         method: 'GET'
-      })
-         .then(response => response.json())
-         .then(response => {
-            // ТАКЖЕ НАДО ПРИ ЗАГРУЗКЕ КОМПОНЕНТА УСТАНОВИТЬ ТЕМУ, КОТОРАЯ СТОИТ У ПОЛЬЗОВАТЕЛЯ
-            setUserName(response['FirstName'])
-            setTheme(response['Theme']) // получить также тему
-         })
-   }, [loggedInUser])
+	useEffect(() => {
+		fetch('http://backend-php/index.php/user/userinfo/' + loggedInUser, {
+			method: 'GET'
+		})
+			.then(response => response.json())
+			.then(response => {
+				// ТАКЖЕ НАДО ПРИ ЗАГРУЗКЕ КОМПОНЕНТА УСТАНОВИТЬ ТЕМУ, КОТОРАЯ СТОИТ У ПОЛЬЗОВАТЕЛЯ
+				setUserName(response['FirstName'])
+				setTheme(response['Theme']) // получить также тему
+			})
+	}, [loggedInUser])
 
-   const onLogOutHandle = () => {
-      setUserName('')
-      onLogOut()
-   }
+	const onLogOutHandle = () => {
+		setUserName('')
+		onLogOut()
+	}
 
-   const handleChangeTheme = () => {
+	const handleChangeTheme = () => {
 
-      // Определяем новое значение темы
-      const newTheme = theme === 'light' ? 'dark' : 'light';
-      setTheme(newTheme)
+		// Определяем новое значение темы
+		const newTheme = theme === 'light' ? 'dark' : 'light';
+		setTheme(newTheme)
 
-      // отправить http запрос на изменении самой темы у текущего пользователя
-      fetch('http://backend/index.php/user/changeTheme/' + loggedInUser, {
-         method: 'POST',
-         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-         },
-         body: JSON.stringify({ Theme: newTheme })
-      })
-         .then(response => response.json())
-         .then(response => console.log(response))
-   }
+		// отправить http запрос на изменении самой темы у текущего пользователя
+		fetch('http://backend-php/index.php/user/changeTheme/' + loggedInUser, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: JSON.stringify({ Theme: newTheme })
+		})
+			.then(response => response.json())
+			.then(response => console.log(response))
+	}
 
-   const changeThemeButtonText = theme === 'light' ? 'Светлая тема' : 'Темная тема';
+	const changeThemeButtonText = theme === 'light' ? 'Светлая тема' : 'Темная тема';
 
-   return (
-      <div className={theme === 'dark' ? 'darkBackground' : ''}>
-         <div className={"header " + (theme === 'dark' ? "darkHeader" : '')}>
-            <div onClick={handleChangeTheme} className={"header__theme " + (theme === 'dark' ? "darkButton" : '')}>{changeThemeButtonText}</div>
-            <div onClick={onLogOutHandle} className={"header__exit " + (theme === 'dark' ? "darkButton" : '')}>Выйти</div>
-         </div>
-         <div className={"text " + (theme === 'dark' ? 'darkText' : '')}>Добро пожаловать в личный кабинет, {userName}!</div>
-      </div>
-   )
+	return (
+		<div className={theme === 'dark' ? 'darkBackground' : ''}>
+			<div className={"header " + (theme === 'dark' ? "darkHeader" : '')}>
+				<div onClick={handleChangeTheme} className={"header__theme " + (theme === 'dark' ? "darkButton" : '')}>{changeThemeButtonText}</div>
+				<div onClick={onLogOutHandle} className={"header__exit " + (theme === 'dark' ? "darkButton" : '')}>Выйти</div>
+			</div>
+			<div className={"text " + (theme === 'dark' ? 'darkText' : '')}>Добро пожаловать в личный кабинет, {userName}!</div>
+		</div>
+	)
 }
 
 export default PersonalAccount
